b92c7e994026ac405989e8c70bc153cd0f1db374
Apple <havubknp@gmail.com>
2021-12-30 17:22:56 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -7,98 +7,95 @@ import Definitions from "./components/Definitions/Definitions";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
-import Webcam from "react-webcam";
-
 function App() {
-  // const [word, setWord] = useState("");
-  // const [meanings, setMeanings] = useState([]);
-  // const [category, setCategory] = useState("en");
-  // const [LightTheme, setLightTheme] = useState(false);
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en");
+  const [LightTheme, setLightTheme] = useState(false);
 
-  // const dictionaryApi = async () => {
-  //   try {
-  //     const data = await axios.get(
-  //       category === "en"
-  //         ? `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
-  //         : `https://api.tracau.vn/WBBcwnwQpV89/s/${word}/${
-  //             category === "en - vi" ? "en" : "vi"
-  //           }`
-  //     );
-  //     setMeanings(category === "en" ? data.data : data.data.sentences);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        category === "en"
+          ? `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+          : `https://api.tracau.vn/WBBcwnwQpV89/s/${word}/${
+              category === "en - vi" ? "en" : "vi"
+            }`
+      );
+      setMeanings(category === "en" ? data.data : data.data.sentences);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // console.log(meanings);
+  console.log(meanings);
 
-  // useEffect(() => {
-  //   word && dictionaryApi();
-  //   // eslint-disable-next-line
-  // }, [word, category]);
+  useEffect(() => {
+    word && dictionaryApi();
+    // eslint-disable-next-line
+  }, [word, category]);
 
-  // const PurpleSwitch = withStyles({
-  //   switchBase: {
-  //     color: grey[50],
-  //     "&$checked": {
-  //       color: grey[900],
-  //     },
-  //     "&$checked + $track": {
-  //       backgroundColor: grey[500],
-  //     },
-  //   },
-  //   checked: {},
-  //   track: {},
-  // })(Switch);
+  const PurpleSwitch = withStyles({
+    switchBase: {
+      color: grey[50],
+      "&$checked": {
+        color: grey[900],
+      },
+      "&$checked + $track": {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
 
   return (
-    // <div
-    //   className="App"
-    //   style={{
-    //     height: "100vh",
-    //     backgroundColor: LightTheme ? "#fff" : "#282c34",
-    //     color: LightTheme ? "black" : "white",
-    //     transition: "all 0.5s linear",
-    //   }}
-    // >
-    //   <Container
-    //     maxWidth="md"
-    //     style={{
-    //       display: "flex",
-    //       flexDirection: "column",
-    //       height: "100vh",
-    //       justifyContent: "space-evenly",
-    //     }}
-    //   >
-    //     <div
-    //       style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
-    //     >
-    //       <span>Chế độ {LightTheme ? "tối" : "sáng"}</span>
-    //       <PurpleSwitch
-    //         checked={LightTheme}
-    //         onChange={() => setLightTheme(!LightTheme)}
-    //       />
-    //     </div>
-    //     <Header
-    //       setWord={setWord}
-    //       category={category}
-    //       setCategory={setCategory}
-    //       word={word}
-    //       setMeanings={setMeanings}
-    //       LightTheme={LightTheme}
-    //     />
-    //     {meanings && (
-    //       <Definitions
-    //         meanings={meanings}
-    //         word={word}
-    //         LightTheme={LightTheme}
-    //         category={category}
-    //       />
-    //     )}
-    //   </Container>
-    //   <Footer />
-    // </div>
-    <Webcam />
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        backgroundColor: LightTheme ? "#fff" : "#282c34",
+        color: LightTheme ? "black" : "white",
+        transition: "all 0.5s linear",
+      }}
+    >
+      <Container
+        maxWidth="md"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+        >
+          <span>Chế độ {LightTheme ? "tối" : "sáng"}</span>
+          <PurpleSwitch
+            checked={LightTheme}
+            onChange={() => setLightTheme(!LightTheme)}
+          />
+        </div>
+        <Header
+          setWord={setWord}
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setMeanings={setMeanings}
+          LightTheme={LightTheme}
+        />
+        {meanings && (
+          <Definitions
+            meanings={meanings}
+            word={word}
+            LightTheme={LightTheme}
+            category={category}
+          />
+        )}
+      </Container>
+      <Footer />
+    </div>
   );
 }
 
